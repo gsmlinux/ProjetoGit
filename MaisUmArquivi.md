cbc24411b2c5a974d616fbf33b26f1407fd8c4c6
Gabriel Matheus <gsmlinux@outlook.com>
2024-06-16 15:48:52 -0300

    Alteração do projeto

---
+++ MaisUmArquivi.md
@@ -0,0 +1,1 @@
+Mais um arquivo enviado pelo git!!!
--- Teste.java
+++ Teste.java
@@ -1,5 +1,16 @@
+import java.util.Scanner;
+
 public class Teste {
     public static void main (String[] args) {
         System.out.println("Olá terminal do computador!!!");
+
+        Scanner entrada = new Scanner(System.in);
+
+        System.out.println("Insira sua idade");
+        double idade = entrada.nextDouble();
+
+        System.out.println("Sua idade é " + idade + " anos.");
+
+        entrada.close();
     }
 }
